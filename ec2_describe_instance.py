abf3bb61c470c9caeacdd2fd6cedf2557c6c0be5
Makendran G <makendran2015@gmail.com>
2022-09-08 10:24:24 +0530

    Update ec2_describe_instance.py

--- ec2_describe_instance.py
+++ ec2_describe_instance.py
@@ -3,5 +3,5 @@ import boto3
 # Creating a client connection with AWS EC2 
 ec2 = boto3.client('ec2')
 # Using the Describe_Instance Method along with filters
-instance = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['KCDCHENNAI-DEMO']}])
+instance = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['BOTO3-DEMO']}])
 print(instance)
